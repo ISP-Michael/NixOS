51cac7d36f852a5ea324938a06f9b0af47503744
Michael <mtr.mikhail090905@gmail.com>
2025-07-24 20:00:00 +0300

    Flake: successful update (nixpkgs, stylix)

--- user/home.nix
+++ user/home.nix
@@ -1,26 +1,7 @@
 {
-  pkgs,
   self,
   ...
 }:
-let
-  pythonEnv13 = pkgs.python313.withPackages(
-    ps: with ps; [
-      cython
-      debugpy
-      django
-      fastapi
-      fastapi-cli
-      fontforge
-      nox
-      nuitka
-      numpy
-      pandas
-      pillow
-      requests
-    ]
-  );
-in
 {
   home = {
     username = "Michael";
@@ -28,20 +9,7 @@ in
     homeDirectory = "/home/Michael";
     sessionVariables = {
       EDITOR = "nvim";
-      NIXOS_OZONE_WL = "1";
-      KDE_NO_OPENGL_WINDOW_DECORATION = "1";
-      QT_WAYLAND_DISABLE_WINDOW_DECORATION = "1";
     };
-    packages = with pkgs; [
-      imv
-      libsForQt5.qtstyleplugins
-      mpv
-      nautilus
-      neovim
-      pythonEnv13
-      telegram-desktop
-      xfce.thunar
-    ];
     file = {
       ".local/share/fonts/literata" = {
         source = "${self}/dependencies/fonts/Literata";
