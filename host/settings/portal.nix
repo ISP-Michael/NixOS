478ad7300f5f2eefd74c5f25fe60ba68b7d84e29
Michael <mtr.mikhail090905@gmail.com>
2025-12-23 15:48:50 +0300

    Add portals for kde & kdialog

--- host/settings/portal.nix
+++ host/settings/portal.nix
@@ -8,10 +8,12 @@
       enable = true;
       extraPortals = with pkgs; [
         xdg-desktop-portal-hyprland
+        kdePackages.xdg-desktop-portal-kde
         xdg-desktop-portal-gtk
       ];
       config = {
-        "org.freedesktop.impl.portal.ScreenCast" = "hyprland";
+        "org.freedesktop.impl.portal.ScreenCast" = [ "hyprland" ];
+        "org.freedesktop.impl.portal.FileChooser" = [ "kde" ];
       };
     };
   };
